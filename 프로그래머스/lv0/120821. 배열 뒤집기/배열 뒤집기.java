3d48bc67ad17535e1c7bd1c1404038ccd2832295
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-01-26 13:36:56 +0900

    [level 0] Title: 배열 뒤집기, Time: 4.83 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,12 +1,13 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
 class Solution {
-    public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
-        int x = 0;
-        
-        for (int i : num_list) {
-            x++;
-            answer[num_list.length - x] = i;
-        }
-        return answer;
+    public int[] solution(int[] numList) {
+        List<Integer> list = Arrays.stream(numList).boxed().collect(Collectors.toList());
+
+        Collections.reverse(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
