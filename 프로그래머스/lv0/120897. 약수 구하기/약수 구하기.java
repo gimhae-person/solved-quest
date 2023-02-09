dff852fac2fee3e730a1d85a39a91b568f1c01b0
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-02-09 17:42:07 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0){
+                sum++;
+            }
+        }
+        
+        int[] answer = new int[sum];
+        int j = 0;
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0){
+                answer[j] = i;
+                j++;
+            }
+        }        
+        
+        return answer;
+    }
+}
