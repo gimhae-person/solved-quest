f09593fdd7280d304df4332b917e6d079ddc0597
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-02-10 22:40:50 +0900

    [level 0] Title: 대문자와 소문자, Time: 14.75 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        char[] str = (my_string).toCharArray();
+        for(int i = 0; i < str.length; i++) {
+            if(Character.isUpperCase(str[i])) answer += Character.toLowerCase(str[i]);
+            else answer += Character.toUpperCase(str[i]);
+        }
+        return answer;
+    }
+}
