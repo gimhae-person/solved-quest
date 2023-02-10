0dd12221b46fb24d269088a71486ddf42e0e0126
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-02-10 14:44:16 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.45 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 1;
+        Arrays.sort(sides);
+        
+        if(sides[2] >= sides[1] + sides[0]) answer = 2;
+        
+        return answer;
+    }
+}
