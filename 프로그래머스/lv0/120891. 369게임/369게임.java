be33d95d7666d7db57a6690b4a5ce1d13520ec31
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-02-12 15:15:59 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        String str = order + "";
         
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == '3' || str.charAt(i) == '6' || str.charAt(i) == '9') {
-                answer ++;
+        while(order > 0) {
+            if(order % 10 == 3 || order % 10 == 6 || order % 10 == 9) {
+                answer++;
             }
+            order = order / 10;
         }
         return answer;
     }
