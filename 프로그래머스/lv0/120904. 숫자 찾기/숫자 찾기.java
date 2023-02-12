30c0eabf2bde14879fc1ad30d55e94c13679080e
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-02-12 15:41:29 +0900

    [level 0] Title: 숫자 찾기, Time: 7.68 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -3,9 +3,10 @@ class Solution {
         int answer = -1;
         String n = num + "";
         
-        for(int i = 0; i < n.length(); i++) {
-            if((n.charAt(i) + "").equals(k + "")) answer = n.indexOf(k + "") + 1;
+        if(n.contains(k + "")) {
+            answer = n.indexOf(k + "") + 1;    
         }
+        
         return answer;
     }
 }
