2127b56eeb9846009bc679adc9fb73a4069b1886
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-02-13 01:18:04 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.40 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -4,10 +4,9 @@ class Solution {
     public long solution(String numbers) {
         long answer = 0;
         String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String[] number = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         
         for(int i = 0; i < str.length; i++) {
-            numbers = numbers.replaceAll(str[i], number[i]);
+            numbers = numbers.replaceAll(str[i], String.valueOf(i));
         }
         
         answer = Long.parseLong(numbers);
