afead4317a5eff669a274ecb3cdc2d804dd3170a
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-02-14 16:57:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -3,15 +3,7 @@ import java.util.*;
 class Solution {
     public long solution(long n) {
         long answer = -1;
-        int i = 1;
-        
-        while(Math.pow(i, 2) <= n) {
-            if(Math.pow(i, 2) == n) {
-                answer = (long)Math.sqrt(n) + 1;
-                answer *= answer;
-            }
-            i++;
-        } 
+        if(Math.pow((int)Math.sqrt(n), 2) == n) answer = (long)Math.pow((int)Math.sqrt(n) + 1, 2);
         return answer;
     }
 }
