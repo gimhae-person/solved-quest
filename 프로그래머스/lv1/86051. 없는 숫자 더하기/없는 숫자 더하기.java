9220126d9abd324b9367b705e6957f3c7eef3cd7
Hyein Kim <110888559+gimhae-person@users.noreply.github.com>
2023-02-22 14:21:59 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,10 +2,8 @@ class Solution {
     public int solution(int[] numbers) {
         int answer = 45;
         
-        for(int i = 0; i <= 9; i++) {
-            for(int j = 0; j < numbers.length; j++) {
-                if(numbers[j] == i) answer -= i;
-            }
+        for(int i = 0; i < numbers.length; i++) {
+            answer -= numbers[i];
         }
         return answer;
     }
